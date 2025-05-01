41addd9c87ed897aabcd4c1ddb4b7bb98dd00fbe
chwadley <chwadley@s.sfusd.edu>
2025-05-01 10:44:52 -0700

    second try

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -13,7 +13,7 @@ public class APCalendar
   public static int numberOfLeapYears(int year1, int year2)
   { 
     int sum = 0;
-    for (int i=year1;i<year2;i++) {
+    for (int i=year1;i<=year2;i++) {
       sum+=isLeapYear(i)?1:0;
     }
     return sum;
